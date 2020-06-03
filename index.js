74618896d9f4db46853bc95d51fc534fee5594f0
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-03 15:36:28 -0400

    Did project setup, got server running on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+const express = require("express");
+
+const server = express();
+
+//middleware
+server.use(express.json());
+
+server.listen(4000, () => {
+    console.log("Server Running on port 4000");
+})
